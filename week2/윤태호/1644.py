38dea18220300a3032607bb93bc8a52fde385eca
taeho0888 <taeho0888@naver.com>
2023-07-02 19:24:15 +0900

    윤태호 1644

--- week2/윤태호/1644.py
+++ week2/윤태호/1644.py
@@ -16,76 +16,55 @@
 # 첫째 줄에 자연수 N을 연속된 소수의 합으로 나타낼 수 있는 경우의 수를 출력한다.
 
 N = int(input())
-count = 0
+prime_list = list()
 
 
-def is_prime(N):
-    if N <= 1:
-        return False
+def find_primes(N):
+    prime_check = [True] * (N+1)
+    prime_check[0] = prime_check[1] = False
 
-    elif N <= 3:
-        return True
-
-    elif N % 2 == 0 or N % 3 == 0:
-        return False
-
-    i = 5
-    while i*i <= N:
-        if N % i == 0 or N % (i+2) == 0:
-            return False
-        i += 6
-
-    return True
+    p = 2
+    while p * p <= N:
+        if prime_check[p]:
+            for i in range(p*p, N+1, p):
+                prime_check[i] = False
+        p += 1
+    for i in range(2, N+1):
+        if prime_check[i]:
+            prime_list.append(i)
 
 
-def next_prime(N):
-    i = N + 1
+def count_sum(prime_list):
+    if not prime_list:  # prime_list가 비어 있으면 0을 출력하고 함수를 종료합니다.
+        print(0)
+        return
+
+    count = 0
+    start = 0
+    end = 0
+    sum = prime_list[0]
+
     while True:
-        if is_prime(i):
-            return i
+        if start > end:
+            break
+
+        if sum < N:
+            end += 1
+            if end >= len(prime_list):
+                break
+            sum += prime_list[end]
+
+        elif sum > N:
+            sum -= prime_list[start]
+            start += 1
+
         else:
-            i += 1
+            count += 1
+            sum -= prime_list[start]
+            start += 1
+
+    print(count)
 
 
-#   start_prime = 시작점
-sum = 0
-start_prime = i = 2
-
-#   end_prime = sum <= N인 끝점
-while sum <= N:
-    sum += i
-
-    if sum <= N:
-        i = next_prime(i)
-    else:
-        break
-
-end_prime = i
-
-print(f"start_prime = {start_prime}\nend_prime={end_prime}\nsum={sum}")
-
-#   무한루프
-while True:
-    if start_prime >= end_prime:
-        break
-
-    if sum == N:
-        count += 1
-
-    if sum >= N:
-        sum -= start_prime
-        start_prime = next_prime(start_prime)
-        print(f"빼기 / sum={sum} / start_prime={start_prime}")
-        continue
-
-    if sum <= N:
-        end_prime = next_prime(end_prime)
-        sum += end_prime
-        print(f"더하기 / sum={sum} / end_prime={end_prime}")
-        continue
-
-
-if is_prime(N):
-    count += 1
-
-print(count)
+find_primes(N)
+count_sum(prime_list)
