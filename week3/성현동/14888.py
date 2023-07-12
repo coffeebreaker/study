fe0d155dcd36dca402b5927d77afa910c8fef976
hdsung98 <hdsung98@naver.com>
2023-07-12 19:22:56 +0900

    week3(5/6):코드 바뀜 수정

--- week3/성현동/14888.py
+++ week3/성현동/14888.py
@@ -1,19 +1,38 @@
+import itertools
 import sys
 
 input = sys.stdin.readline
 
-mushroom_list = []
-for _ in range(10):
-    mushroom = int(input().strip())
-    mushroom_list.append(mushroom)
 
-total = 0
-end = len(mushroom_list)
-for i in range(end):
-    one_more = total + mushroom_list[i]
-    if one_more - 100 <= 100 - total:
-        total = one_more
-    else:
-        break
+def calc(num1, num2, op):
+    if op == '+':
+        return num1 + num2
+    elif op == '-':
+        return num1 - num2
+    elif op == '*':
+        return num1 * num2
+    elif op == '/':
+        if num1 < 0:
+            return -(-num1 // num2)
+        else:
+            return num1 // num2
 
-print(total)
+
+num_size = int(input())
+num_list = [int(i) for i in input().split()]
+
+ops = [int(j) for j in input().split()]
+op_list = ['+'] * ops[0] + ['-'] * ops[1] + ['*'] * ops[2] + ['/'] * ops[3]
+op_perm_list = list(set(itertools.permutations(op_list)))
+
+
+total_list = []
+for op_perm in op_perm_list:
+    cur_total = num_list[0]
+    for num, op in zip(num_list[1:], op_perm):
+        cur_total = calc(cur_total, num, op)
+    total_list.append(cur_total)
+
+print(max(total_list))
+print(min(total_list))
+
