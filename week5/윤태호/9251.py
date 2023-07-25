949a12dfe2548bfe6382417dc3ac6ece468a1275
taeho0888 <taeho0888@naver.com>
2023-07-25 23:07:42 +0900

    윤태호 9251

--- week5/윤태호/9251.py
+++ week5/윤태호/9251.py
@@ -9,13 +9,16 @@
 # 출력
 # 첫째 줄에 입력으로 주어진 두 문자열의 LCS의 길이를 출력한다.
 
-str1 = list(input())
-str2 = list(input())
+str1 = input()
+str2 = input()
 
-n = max(len(str1), len(str2))
+dp = [[0] * (len(str2) + 1) for _ in range(len(str1) + 1)]
 
-dp = [0]*n
+for i in range(1, len(str1) + 1):
+    for j in range(1, len(str2) + 1):
+        if str1[i-1] == str2[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
-for i in range(1,n):
-    for j in range(i):
-        if
+print(dp[-1][-1])
