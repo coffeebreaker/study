1ad26e34b3e2e47bef6a4c70555ac3abe7a37951
woochan <wchan409@naver.com>
2023-08-02 14:32:21 +0900

    오?

--- week6/이우찬/7562.py
+++ week6/이우찬/7562.py
@@ -1,4 +1,4 @@
-#실버1
+#실버1 정답!
 # 문제
 # 체스판 위에 한 나이트가 놓여져 있다. 나이트가 한 번에 이동할 수 있는 칸은 아래 그림에 나와있다. 나이트가 이동하려고 하는 칸이 주어진다. 나이트는 몇 번 움직이면 이 칸으로 이동할 수 있을까?
 
@@ -11,3 +11,52 @@
 
 # 출력
 # 각 테스트 케이스마다 나이트가 최소 몇 번만에 이동할 수 있는지 출력한다.
+
+# 1 0 0 0 2 0 0 0
+# 0 0 1 0 0 0 0 0
+# 2 1 0 0 2 0 0 0
+# 0 2 0 2 0 0 0 0
+# 0 0 0 0 0 0 0 0
+# 0 0 0 0 0 0 0 0
+# 0 0 0 0 0 0 0 0
+# 2 0 0 0 0 0 0 0
+
+dx = [-1, -2, -2, -1, 1, 2, 2, 1]
+dy = [-2, -1, 1, 2, 2, 1, -1, -2]
+
+def bfs(a,b):
+    # visited = [[False for _ in range(M)] for _ in range(N)]
+    q = []
+    q.append([a,b])
+
+    while q:
+        x,y = q.pop(0)
+        for i in range(8):
+            #8방위 시작
+            nx, ny = x + dx[i], y + dy[i]
+            if 0<=nx<L and 0<=ny<L and matrix[nx][ny]!=1:
+                if matrix[nx][ny] == 2:
+                    res = count[x][y] + 1
+                    return res
+                # 안만나면 증가
+                else:
+                    matrix[nx][ny] = 1
+                    q.append([nx,ny])
+                    count[nx][ny] = count[x][y] + 1
+
+#input
+T = int(input())
+for _ in range(T):
+    L = int(input())
+    matrix = [[0 for _ in range(L)] for _ in range(L)]
+    x,y = map(int, input().split())
+    matrix[x][y] = 1
+    z,w = map(int, input().split())
+    if x==z and y==w:
+        print(0)
+        pass
+    else:
+        matrix[z][w] = 2
+        count = [[0 for _ in range(L)] for _ in range(L)]
+        res = bfs(x,y)
+        print(res)
