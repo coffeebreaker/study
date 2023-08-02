113647ff77f6c093a04de740efbdaf5221abd405
woochan <wchan409@naver.com>
2023-08-02 11:39:08 +0900

    여기가 한곈가

--- week6/이우찬/11724.py
+++ week6/이우찬/11724.py
@@ -1,13 +1,50 @@
-#실버1
+#실버2 시간초과남 근데 틀린건 모르겠음
 # 문제
-# 체스판 위에 한 나이트가 놓여져 있다. 나이트가 한 번에 이동할 수 있는 칸은 아래 그림에 나와있다. 나이트가 이동하려고 하는 칸이 주어진다. 나이트는 몇 번 움직이면 이 칸으로 이동할 수 있을까?
-
-
+# 방향 없는 그래프가 주어졌을 때, 연결 요소 (Connected Component)의 개수를 구하는 프로그램을 작성하시오.
 
 # 입력
-# 입력의 첫째 줄에는 테스트 케이스의 개수가 주어진다.
-
-# 각 테스트 케이스는 세 줄로 이루어져 있다. 첫째 줄에는 체스판의 한 변의 길이 l(4 ≤ l ≤ 300)이 주어진다. 체스판의 크기는 l × l이다. 체스판의 각 칸은 두 수의 쌍 {0, ..., l-1} × {0, ..., l-1}로 나타낼 수 있다. 둘째 줄과 셋째 줄에는 나이트가 현재 있는 칸, 나이트가 이동하려고 하는 칸이 주어진다.
+# 첫째 줄에 정점의 개수 N과 간선의 개수 M이 주어진다. (1 ≤ N ≤ 1,000, 0 ≤ M ≤ N×(N-1)/2) 둘째 줄부터 M개의 줄에 간선의 양 끝점 u와 v가 주어진다. (1 ≤ u, v ≤ N, u ≠ v) 같은 간선은 한 번만 주어진다.
 
 # 출력
-# 각 테스트 케이스마다 나이트가 최소 몇 번만에 이동할 수 있는지 출력한다.
+# 첫째 줄에 연결 요소의 개수를 출력한다.
+
+# 1 2 3 4 5 6
+# 2 1 4 3 2 4
+# 5 5   6
+
+# 1 2 5
+# 3 4 6
+
+import sys
+sys.setrecursionlimit(10**6)
+
+def dfs(c,count):
+    visited[c] = True
+    count[0] += 1
+    for i in dots[c]:
+        if visited[i]==False:
+            count[0] += 1
+            dfs(i,count)
+
+N, M = map(int, input().split())
+
+dots = [[] for _ in range(N+1)]
+for i in range(M):
+    a, b = map(int, input().split())
+    dots[a].append(b)
+    dots[b].append(a)
+for i in range(1, N+1):
+    dots[i].sort()
+
+visited = [False] * (N+1)
+res = 0
+
+count = [0]
+for c in range(1, N+1):
+    count[0] = 0 
+    if visited[c]==False:
+        dfs(c,count)
+    if count[0]!=0:
+        res += 1
+
+print(res)
--- week6/이우찬/17086.py
+++ week6/이우찬/17086.py
@@ -1,15 +1,119 @@
-#골드4
+#실버2 맞았당
 # 문제
-# 수행해야 할 작업 N개 (3 ≤ N ≤ 10000)가 있다. 각각의 작업마다 걸리는 시간(1 ≤ 시간 ≤ 100)이 정수로 주어진다.
+# N×M 크기의 공간에 아기 상어 여러 마리가 있다. 공간은 1×1 크기의 정사각형 칸으로 나누어져 있다. 한 칸에는 아기 상어가 최대 1마리 존재한다.
 
-# 몇몇 작업들 사이에는 선행 관계라는 게 있어서, 어떤 작업을 수행하기 위해 반드시 먼저 완료되어야 할 작업들이 있다. 이 작업들은 번호가 아주 예쁘게 매겨져 있어서, K번 작업에 대해 선행 관계에 있는(즉, K번 작업을 시작하기 전에 반드시 먼저 완료되어야 하는) 작업들의 번호는 모두 1 이상 (K-1) 이하이다. 작업들 중에는, 그것에 대해 선행 관계에 있는 작업이 하나도 없는 작업이 반드시 하나 이상 존재한다. (1번 작업이 항상 그러하다)
+# 어떤 칸의 안전 거리는 그 칸과 가장 거리가 가까운 아기 상어와의 거리이다. 두 칸의 거리는 하나의 칸에서 다른 칸으로 가기 위해서 지나야 하는 칸의 수이고, 이동은 인접한 8방향(대각선 포함)이 가능하다.
 
-# 모든 작업을 완료하기 위해 필요한 최소 시간을 구하여라. 물론, 서로 선행 관계가 없는 작업들은 동시에 수행 가능하다.
+# 안전 거리가 가장 큰 칸을 구해보자. 
 
 # 입력
-# 첫째 줄에 N이 주어진다.
-
-# 두 번째 줄부터 N+1번째 줄까지 N개의 줄이 주어진다. 2번째 줄은 1번 작업, 3번째 줄은 2번 작업, ..., N+1번째 줄은 N번 작업을 각각 나타낸다. 각 줄의 처음에는, 해당 작업에 걸리는 시간이 먼저 주어지고, 그 다음에 그 작업에 대해 선행 관계에 있는 작업들의 개수(0 ≤ 개수 ≤ 100)가 주어진다. 그리고 선행 관계에 있는 작업들의 번호가 주어진다.
+# 첫째 줄에 공간의 크기 N과 M(2 ≤ N, M ≤ 50)이 주어진다. 둘째 줄부터 N개의 줄에 공간의 상태가 주어지며, 0은 빈 칸, 1은 아기 상어가 있는 칸이다. 빈 칸과 상어의 수가 각각 한 개 이상인 입력만 주어진다.
 
 # 출력
-# 첫째 줄에 모든 작업을 완료하기 위한 최소 시간을 출력한다.
+# 첫째 줄에 안전 거리의 최댓값을 출력한다.
+
+# input
+# 0 0 1 0
+# 0 0 0 0
+# 1 0 0 0
+# 0 0 0 0
+# 0 0 0 1
+
+# filtered
+# 0 2 2 2
+# 2 2 2 2
+# 2 2 0 0
+# 2 2 2 2
+# 0 0 2 2
+
+# res
+# 2 0 0 0
+# 0 0 0 0
+# 0 0 2 2
+# 0 0 0 0
+# 2 2 0 0
+
+dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+def bfs(a,b):
+    distance = [[0 for _ in range(M)] for _ in range(N)]
+    visited = [[False for _ in range(M)] for _ in range(N)]
+    q = []
+    q.append([a,b])
+
+    while q:
+        x,y = q.pop(0)
+        for i in range(8):
+            #8방위 시작
+            nx, ny = x + dx[i], y + dy[i]
+            if 0<=nx<N and 0<=ny<M and not visited[nx][ny]:
+                # 상어 만나면 그게 최소거리값
+                if matrix[nx][ny] == 1:
+                    res[a][b] = distance[x][y] + 1
+                    return
+                # 안만나면 증가
+                else:
+                    visited[nx][ny] = True
+                    q.append([nx,ny])
+                    distance[nx][ny] = distance[x][y] + 1
+
+
+# 다 상어 방지용
+def one(matrix):
+    for row in matrix:
+        for element in row:
+            if element != 1:
+                return False
+    return True
+
+# filter로 다 걸러진 경우
+def two(matrix):
+    for row in matrix:
+        for element in row:
+            if element != 2:
+                return False
+    return True
+
+#input
+N,M = map(int, input().split())
+matrix = [[0 for _ in range(M)] for _ in range(N)]
+for i in range(N):
+    input_line = input().split()
+    for j in range(M):
+        matrix[i][j] = int(input_line[j])
+
+# 미리 예방
+if one(matrix):
+    print(0)
+    exit()
+
+# 상어위치랑 상어 테두리는 다 제껴버리는 filtered. (탐색 줄이기 위함 = 만약 다 제껴지면 안전거리 1 1밖에 없으면 0임)
+filtered = [[0 for _ in range(M)] for _ in range(N)]
+
+for i in range(N):
+    for j in range(M):
+        if matrix[i][j] == 1:
+            filtered[i][j] = 2
+            # 8방향을 검사하여 주변의 0을 2로 바꿈
+            for k in range(8):
+                nx, ny = i + dx[k], j + dy[k]
+                if 0 <= nx < N and 0 <= ny < M and matrix[nx][ny] == 0:
+                    filtered[nx][ny] = 2
+
+# 안전거리 1 예방
+if two(filtered):
+    print(1)
+    exit()
+
+# 거리들을 담는 res
+res = [[0 for _ in range(M)] for _ in range(N)]
+
+#bfs 돌리자
+for i in range(N):
+    for j in range(M):
+        if filtered[i][j] == 0:
+            bfs(i,j)
+
+max_value = max(max(row) for row in res)
+print(max_value)
