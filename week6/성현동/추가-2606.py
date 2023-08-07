c42c8ebd3b8004068faa3c934fd2709845a4794e
hdsung98 <hdsung98@naver.com>
2023-08-08 02:26:49 +0900

    week6: bfs 보충(+5)

--- week6/성현동/추가-2606.py
+++ week6/성현동/추가-2606.py
@@ -1,34 +1,26 @@
 from collections import deque
 
-directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-
-
-def calc_worm(i, j):
-    q = deque([(i, j)])
-    visited[i][j] = True
-    while q:
-        y, x = q.popleft()
-        for dy, dx in directions:
-            new_y, new_x = y + dy, x + dx
-            if not (0 <= new_y < row and 0 <= new_x < col):
-                continue
-            if not visited[new_y][new_x] and field[y][x] == 1:
-                visited[new_y][new_x] = True
-                q.append((new_y, new_x))
-
-
+computer_num = int(input())
+# 엣지 정보가 주어지면, 그래프는 딕셔너리로 관리하는게 용이
+graph = {i: [] for i in range(1, computer_num + 1)}
 for _ in range(int(input())):
-    col, row, planted = map(int, input().split())
-    field = [[0] * col for _ in range(row)]
-    for _ in range(planted):
-        x, y = map(int, input().split())
-        field[y][x] = 1
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-    visited = [[False] * col for _ in range(row)]
-    cnt = 0
-    for y in range(row):
-        for x in range(col):
-            if not visited[y][x] and field[y][x] == 1:
-                calc_worm(y, x)
-                cnt += 1
-    print(cnt)
+q = deque([1])
+visited = [False] * (computer_num + 1)
+cnt = 0
+while q:
+    node = q.popleft()
+    visited[node] = True
+    cnt += 1
+    for neighbor in graph[node]:
+        if not visited[neighbor]:
+            # 미방문 노드 추가 전 반드시 방문처리할 것!
+            # 한 노드가 이 노드보다 먼저 pop된 노드의 neighbor인 경우
+            # 중복해서 큐에 추가 되는 현상 발생할 수 있기 때문
+            visited[neighbor] = True
+            q.append(neighbor)
+
+print(cnt - 1)
